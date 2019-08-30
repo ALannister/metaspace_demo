01a456113e3f7891be5c0d14f395c7dacde54612
Allen <32953986+ALannister@users.noreply.github.com>
2019-08-30 11:16:07 +0800

    Initial commit

---
+++ src/main/java/com/lannister/java/demo/oom/MetaspaceDemo.java
@@ -0,0 +1,37 @@
+package com.lannister.java.demo.oom;
+
+
+import java.lang.reflect.Method;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+//添加VM参数: -XX:MetaspaceSize=8m -XX:MaxMetaspaceSize=8m
+public class MetaspaceDemo {
+	static class OOMTest{}
+	public static void main(String[] args) {
+		int i = 0;
+		
+		try {
+			while(true) {
+				i++;
+				Enhancer enhancer = new Enhancer();
+				enhancer.setSuperclass(OOMTest.class);
+				enhancer.setUseCache(false);
+				enhancer.setCallback(new MethodInterceptor() {
+					
+					public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+						// TODO Auto-generated method stub
+						return proxy.invokeSuper(obj, args);
+					}
+				});
+				enhancer.create();
+			}
+		} catch (Throwable e) {
+			// TODO: handle exception
+			System.out.println("************多少次后发生了错误： " + i);
+			e.printStackTrace();
+		}
+	}
+}
